8774053c5a3b12fdc4bc9630220ebadf57e953e2
Yisusad <jesuscheoz@gmail.com>
2021-02-06 02:32:36 -0300

    Se crea Sección Galeria

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const sass = require('gulp-sass');
 const imagemin = require('gulp-imagemin');
 const notify = require('gulp-notify');
 const webp = require('gulp-webp');
+const concat = require('gulp-concat');
 
 
 // Funcion para SASS
@@ -15,6 +16,7 @@ function css(){
         .pipe( dest('./build/css') )
 } 
 
+
 function minificarcss(){
     return src('src/scss/app.scss')
         .pipe( sass({
@@ -22,6 +24,12 @@ function minificarcss(){
         }) )
         .pipe( dest('./build/css') )
 } 
+
+function javascript(){
+    return src('src/js/**/*.js')
+    .pipe( concat('bundle.js') )
+    .pipe( dest('./build/js'))
+}
 
 function imagenes(){
     return src('src/img/**/*')
@@ -39,12 +47,14 @@ function versionWebp(){
 
 function watchArchivos(){
     watch('src/scss/**/*.scss', css);  //Todas las carpeta y archivos de Scss
+    watch('src/js/**/*.js', javascript);
 }
 
 exports.css = css;
 exports.minificarcss = minificarcss;
+exports.javascript = javascript;
 exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
 exports.versionWebp = versionWebp;
 
-exports.default = series(css, imagenes, versionWebp, watchArchivos);
+exports.default = series(css, javascript, imagenes, versionWebp, watchArchivos);
